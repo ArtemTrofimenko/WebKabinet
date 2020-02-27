ecfaac319e878a638ad76e9b3d590c35bf6a0b71
Admin <virtu0z13>
2020-02-27 15:43:50 +0200

    multi contragent total weight

--- src/main/java/com.web_kabinet/domain/Operation.java
+++ src/main/java/com.web_kabinet/domain/Operation.java
@@ -7,5 +7,5 @@ import java.io.Serializable;
 
 public enum Operation implements Serializable {
     @Enumerated(EnumType.STRING)
-    COMING, CONSUMPTION, DRYING, CLEANING_DRYING;
+    COMING, CONSUMPTION;
 }
